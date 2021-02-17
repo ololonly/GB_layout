c9bcb308cd9cd9feaca49bf0de2b3fdf93d4dd4f
Иван Костяшов <onlyno21@gmail.com>
2021-02-17 16:44:05 +0300

    Доделал brand добавил анимацию для menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,19 @@
-$(document).ready(() => {
-    // $('body').hide();
-});
+const $menu = $('.brandMenu');
+const $menuButton = $('.barsIcon');
+const $menuClose = $('.menuClose');
+
+
+var GbLayout = {
+    onLoad: () => {
+        $menuButton.on('click', GbLayout.menuToggler);
+        $menuClose.on('click', GbLayout.menuToggler);
+    },
+
+    menuToggler: () => {
+        $menu.toggleClass('menu-open');
+    },
+
+
+}
+
+$(GbLayout.onLoad);
